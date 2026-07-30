5c35bdadc28b0fad07a2a3908b366944c08c5266
Obscrt7870 <cia.ge.1bcomgesl@gmail.com>
2026-07-30 10:33:31 -0400

    Refactor key derivation method in encryption tool
    Refactor key_from_password function to use PBKDF2HMAC for key derivation with a specified salt.

--- Ferramenta de criptografia simétrica de arquivos.py
+++ Ferramenta de criptografia simétrica de arquivos.py
@@ -1,7 +1,7 @@
 import base64, sys from cryptography.hazmat.primitives.kdf.pbkdf2 
 import PBKDF2HMAC from cryptography.hazmat.primitives 
 import hashes from cryptography.fernet import Fernet
-
+ 
 SALT = b"novatech-lab-salt-2024"
 
 def key_from_password(password: str) -> bytes: kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=SALT, iterations=100_000) return base64.urlsafe_b64encode(kdf.derive(password.encode()))
